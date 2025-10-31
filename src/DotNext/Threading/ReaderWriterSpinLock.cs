8b0b574981aae464e6b2197d2c0316cd3b698f92
sakno <roman.sakno@gmail.com>
2025-10-31 20:16:03 +0200

    Split inner types

--- src/DotNext/Threading/ReaderWriterSpinLock.cs
+++ src/DotNext/Threading/ReaderWriterSpinLock.cs
@@ -10,73 +10,10 @@ namespace DotNext.Threading;
 /// This type should not be used to synchronize access to the I/O intensive resources.
 /// </remarks>
 [StructLayout(LayoutKind.Auto)]
-public struct ReaderWriterSpinLock
+public partial struct ReaderWriterSpinLock
 {
-    /// <summary>
-    /// Represents lock stamp used for optimistic reading.
-    /// </summary>
-    [StructLayout(LayoutKind.Auto)]
-    public readonly struct LockStamp : IEquatable<LockStamp>
-    {
-        private readonly uint version;
-        private readonly bool valid;
-
-        internal LockStamp(ref readonly uint version)
-        {
-            this.version = Volatile.Read(in version);
-            valid = true;
-        }
-
-        internal bool IsValid(in uint version) => valid && this.version == Volatile.Read(in version);
-
-        private bool Equals(in LockStamp other)
-            => version == other.version && valid == other.valid;
-
-        /// <summary>
-        /// Determines whether this stamp represents the same version of the lock state
-        /// as the given stamp.
-        /// </summary>
-        /// <param name="other">The lock stamp to compare.</param>
-        /// <returns><see langword="true"/> of this stamp is equal to <paramref name="other"/>; otherwise, <see langword="false"/>.</returns>
-        public bool Equals(LockStamp other) => Equals(in other);
-
-        /// <summary>
-        /// Determines whether this stamp represents the same version of the lock state
-        /// as the given stamp.
-        /// </summary>
-        /// <param name="other">The lock stamp to compare.</param>
-        /// <returns><see langword="true"/> of this stamp is equal to <paramref name="other"/>; otherwise, <see langword="false"/>.</returns>
-        public override bool Equals([NotNullWhen(true)] object? other) => other is LockStamp stamp && Equals(in stamp);
-
-        /// <summary>
-        /// Computes hash code for this stamp.
-        /// </summary>
-        /// <returns>The hash code of this stamp.</returns>
-        public override int GetHashCode() => HashCode.Combine(valid, version);
-
-        /// <summary>
-        /// Determines whether the first stamp represents the same version of the lock state
-        /// as the second stamp.
-        /// </summary>
-        /// <param name="first">The first lock stamp to compare.</param>
-        /// <param name="second">The second lock stamp to compare.</param>
-        /// <returns><see langword="true"/> of <paramref name="first"/> stamp is equal to <paramref name="second"/>; otherwise, <see langword="false"/>.</returns>
-        public static bool operator ==(in LockStamp first, in LockStamp second)
-            => first.Equals(in second);
-
-        /// <summary>
-        /// Determines whether the first stamp represents the different version of the lock state
-        /// as the second stamp.
-        /// </summary>
-        /// <param name="first">The first lock stamp to compare.</param>
-        /// <param name="second">The second lock stamp to compare.</param>
-        /// <returns><see langword="true"/> of <paramref name="first"/> stamp is not equal to <paramref name="second"/>; otherwise, <see langword="false"/>.</returns>
-        public static bool operator !=(in LockStamp first, in LockStamp second)
-            => !first.Equals(in second);
-    }
-
     private const int WriteLockState = int.MinValue;
-    private const int NoLockState = default;
+    private const int NoLockState = 0;
     private const int SingleReaderState = 1;
 
     private volatile int state;
@@ -90,7 +27,7 @@ public struct ReaderWriterSpinLock
     {
         // Ordering of version and lock state must be respected:
         // Write lock acquisition changes the state to Acquired and then increments the version.
-        // Optimistic read lock reads the version and then checks Acquired lock state to avoid false positivies.
+        // Optimistic read lock reads the version and then checks Acquired lock state to avoid false positives.
         var stamp = new LockStamp(in version);
         return state is WriteLockState ? default : stamp;
     }
